e2386fa8169d1ffba5eebea16377bd964f74c960
Thales Cardoso <canadathales@gmail.com>
2024-03-23 13:32:30 -0400

    Typed the `libraries`

---
+++ src/libs/libs.types.ts
@@ -0,0 +1,20 @@
+interface Libs {
+  name: string;
+  Q: string;
+  W: string;
+  E: string;
+  A: string;
+  D: string;
+  Z: string;
+  X: string;
+  C: string;
+  S: string;
+  buttonColor: string;
+  backgroundColor: string;
+  oniEmoji: string;
+}
+
+interface Bank {
+  label: string;
+  sounds: Libs[];
+}
