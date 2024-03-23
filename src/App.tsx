a186d3c5010b4a349bb025735a02b535714fdb6f
Thales Cardoso <canadathales@gmail.com>
2024-03-23 13:29:30 -0400

    Created `APP` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import DrumPad from "./components/drumpad/drumpad";
+import Info from "./components/info/info";
+import Options from "./components/options/options";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div id="drum-machine" className="taiko-container">
+      <div className="bg" />
+      <div className="bg-color" />
+      <div className="drum-pad-container-wrapper">
+        <Info />
+        <div className="drum-pad-container">
+          <div className="drum-row">
+            <DrumPad letter="Q" />
+            <DrumPad letter="W" />
+            <DrumPad letter="E" />
+          </div>
 
-export default App
+          <div className="drum-row-oni">
+            <DrumPad letter="A" />
+            <DrumPad letter="S" />
+            <DrumPad letter="D" />
+          </div>
+
+          <div className="drum-row">
+            <DrumPad letter="Z" />
+            <DrumPad letter="X" />
+            <DrumPad letter="C" />
+          </div>
+        </div>
+      </div>
+      <Options />
+      <span className="icons">
+        Icons by <a href="https://icons8.com">Icons8</a>
+      </span>
+    </div>
+  );
+};
+
+export default App;
